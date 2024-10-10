d902c97300b4dc7baaad9e99af574c0e4d29ccae
giovannitoledo1 <geotoledo805@gmail.com>
2024-10-10 12:45:18 -0700

    Added challenge 1 solution. Application now shows countries visited and total countries.

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Red!133606",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function getVisitedCountries() {
+  try{
+    const res = await db.query("SELECT * FROM visited_countries");
+    return res;
+  } catch(err){
+    console.log("Error retrieving data: " ,err);
+    return [];
+  }
+};
+
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+  // //Write your code here.
+  const visitedCountries = await getVisitedCountries();
+  let countries = [];
+  visitedCountries.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+
+
+  console.log("Visited Countries: ", visitedCountries.rows);
+
+  res.render("index.ejs", { countries: countries, total: countries.length});
+  db.end();
 });
 
 app.listen(port, () => {
